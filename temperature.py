d64ed1dd22ff1157728470a975cbf043ff4bc903
dasgfhewgqhfygehybgvdaf <PE/sports4021ONPAy>
2026-02-01 19:42:45 +0930

    fefg

---
+++ temperature.py
@@ -0,0 +1,6 @@
+temp=int(input("What is the Temperature today?"))
+if temp>15:
+    print("It is suitable to wear light clothing today")
+else:
+    print("It is not suitable to wear light clothing today")
+
